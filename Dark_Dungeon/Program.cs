333931dff4444c9884bfe806a663e1a40cf37a31
EvanC421 <evan.clark1@mohawkcollege.ca>
2025-10-06 14:47:46 -0400

    Add basic Southward movement

--- Dark_Dungeon/Program.cs
+++ Dark_Dungeon/Program.cs
@@ -4,39 +4,54 @@ string[,] map = new string[3,3];
 int playerX = 0;
 int playerY = 0;
 
-//Set every space on the map to empty
-for (int i = 0; i < 3; i++)
+
+
+while (playerX < 3 && playerY < 3)
 {
-    for (int j = 0; j < 3; j++)
+
+    //Set every space on the map to empty
+    for (int i = 0; i < 3; i++)
     {
-        map[i, j] = "[ ]";
+        for (int j = 0; j < 3; j++)
+        {
+            map[i, j] = "[ ]";
+        }
     }
-}
 
-//Fill the players location with an X
-map[playerX, playerY] = "[x]";
 
-//Display every coordinate of the map
-for (int i = 0; i < 3; i++)
-{
-    for (int j = 0; j < 3; j++)
+    String input = Console.ReadLine();
+    if (input.Contains("South"))
     {
-        Console.Write(map[i, j]);
+        playerY += 1;
     }
-    Console.WriteLine();
+
+    //Fill the players location with an X
+    map[playerY, playerX] = "[x]";
+
+    //Display every coordinate of the map
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            Console.Write(map[i, j]);
+        }
+        Console.WriteLine();
+    }
+
+    //Set message for all rooms
+    message = $"You are in {playerX}, {playerY}.";
+
+    //Check specific room
+    if (playerX == 2 && playerY == 3)
+    {
+        message = "You are in room 2,3";
+    }
+
+
+    Console.WriteLine("\n" + message);
+    Console.ReadKey();
+    Console.Clear();
 }
 
-//Set message for all rooms
-message = $"You are in {playerX}, {playerY}.";
-
-//Check specific room
-if (playerX == 2 && playerY == 3)
-{
-    message = "You are in room 2,3";
-}
-
-
-Console.WriteLine("\n" + message);
-Console.ReadKey();
 
 
